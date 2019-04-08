7bdd7f9ee701b9016303b62e04a8a8188308c6ae
alfonsserra <alfonsoserra@gmail.com>
2019-04-08 10:08:32 +0200

    Set the mapper from the DTO

--- src/main/java/com/systelab/spring/controller/SupplierMapper.java
+++ src/main/java/com/systelab/spring/controller/SupplierMapper.java
@@ -3,11 +3,13 @@ package com.systelab.spring.controller;
 import com.systelab.spring.model.Supplier;
 import com.systelab.spring.model.SupplierDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SupplierMapper {
 
     SupplierDTO supplierToDTO(Supplier source);
 
+    @Mapping(target = "contact", ignore = true)
     Supplier supplierFromDTO(SupplierDTO destination);
 }
